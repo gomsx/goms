d4cbf6195065f4612e098588e23fee681c5c6cb8
sfw <fuwens2019@163.com>
2019-09-19 18:59:07 +0800

    new cicd new build ubuntu image dockerfile improve .travis.yaml build.sh dockerfile

--- example/cicd/docker/build.sh
+++ example/cicd/docker/build.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-#echo $PATH;which go # 查看环境变量
-APP_IMAGE="eDocker"
-DOCKER_IMAGE="edocker"
-
-go build -o $APP_IMAGE ../
-ls -l ./$APP_IMAGE ../
-chmod +x ./$APP_IMAGE #重要
+DOCKER_IMAGE="ubuntu"
 
 docker build -t $DOCKER_IMAGE .
-docker run -it $DOCKER_IMAGE
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
 docker tag $DOCKER_IMAGE $DOCKER_USERNAME/$DOCKER_IMAGE
