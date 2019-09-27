5540f5270340533c6f13de4d53f2783d4e3f15db
sfw <fuwens2019@163.com>
2019-09-27 16:45:00 +0800

    add test path

--- eHttp/docker/build.sh
+++ eHttp/docker/build.sh
@@ -6,9 +6,7 @@ go build -o $APP_IMAGE ../cmd
 ls -l ./$APP_IMAGE ../
 chmod +x ./$APP_IMAGE 
 
-# 下面 ../ 表示构建环境(目录)  
 docker build -t $DOCKER_IMAGE -f dockerfile  ../    
-docker run -it $DOCKER_IMAGE
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
 docker tag $DOCKER_IMAGE $DOCKER_USERNAME/$DOCKER_IMAGE
--- eHttp/internal/server/http/server.go
+++ eHttp/internal/server/http/server.go
@@ -1,6 +1,8 @@
 package http
 
 import (
+	"fmt"
+
 	"github.com/fuwensun/goexample/eHttp/internal/service"
 	"github.com/gin-gonic/gin"
 )
@@ -12,5 +14,26 @@ var (
 // New new a bm server.
 func New(s *service.Service) (engine *gin.Engine) {
 	engine = gin.Default()
+	initRouter(engine)
+	engine.Run()
 	return
+}
+
+//
+func initRouter(e *gin.Engine) {
+	g := e.Group("/test")
+	{
+		g.GET("/print", testPrint)
+		g.GET("/ping", testPing)
+	}
+}
+
+// example for http request handler.
+func testPrint(c *gin.Context) {
+	fmt.Println("http server ok!!!")
+}
+func testPing(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
 }
