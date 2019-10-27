e5423bd50c3d0ea78ce4f4c2d3b30ff025a68bab
sfw <fuwens2019@163.com>
2019-10-27 15:25:04 +0800

    new localbuild.sh

---
+++ localbuild.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+set -x
+set -u
+
+pwd; PRODIR=$(pwd);echo $PRODIR;   # 查看环境变量
+echo $HOME; echo $PATH; which go;   
+  
+cd $PRODIR; go test ./...   # all
+  
+cd $PRODIR/eConf/docker; chmod +x ./build.sh; ./build.sh   # eConf
+cd $PRODIR/eGrpc/docker; chmod +x ./build.sh; ./build.sh   # eGrpc
+cd $PRODIR/eHttp/docker; chmod +x ./build.sh; ./build.sh   # eHttp
+cd $PRODIR/eFlag/docker; chmod +x ./build.sh; ./build.sh   # eFlag
+cd $PRODIR/eYaml/docker; chmod +x ./build.sh; ./build.sh   # eYaml
+cd $PRODIR/eDocker/docker; chmod +x ./build.sh; ./build.sh # eDocker 
+cd $PRODIR/cicd/docker; chmod +x ./build.sh; ./build.sh    # ubuntu
+cd $PRODIR/eModule/cmd; go run .                           # eModule
+
+
+
+
+
+
