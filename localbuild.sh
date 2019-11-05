c82c315a573864b666cc613c855cdec899ba8e25
wahaha <xxxfuwen@gmail.com>
2019-11-05 17:44:26 +0800

    Revert "update build system (fix #11)"

--- localbuild.sh
+++ localbuild.sh
@@ -3,14 +3,10 @@ set -e
 set -x
 set -u
 
-# .sh所在目录
-DIR=$(cd "$(dirname "$0")";pwd)
-echo $DIR
-PRODIR=$(dir/..;pwd)
-echo $PRODIR;   
-
-# 查看环境变量
+pwd; PRODIR=$(pwd);echo $PRODIR;   # 查看环境变量
 echo $HOME; echo $PATH; which go;   
+  
+cd $PRODIR; go test ./...   # all
   
 cd $PRODIR/eMysql/docker; chmod +x ./build.sh; ./build.sh  # eMysql
 cd $PRODIR/eConf/docker; chmod +x ./build.sh; ./build.sh   # eConf
@@ -19,10 +15,10 @@ cd $PRODIR/eHttp/docker; chmod +x ./build.sh; ./build.sh   # eHttp
 cd $PRODIR/eFlag/docker; chmod +x ./build.sh; ./build.sh   # eFlag
 cd $PRODIR/eYaml/docker; chmod +x ./build.sh; ./build.sh   # eYaml
 cd $PRODIR/eDocker/docker; chmod +x ./build.sh; ./build.sh # eDocker 
-
+cd $PRODIR/cicd/docker; chmod +x ./build.sh; ./build.sh    # ubuntu
 cd $PRODIR/eModule/cmd; go run .                           # eModule
 
-cd $PRODIR/build/cicd/docker; chmod +x ./build.sh; ./build.sh    # ubuntu
+
 
 
 
