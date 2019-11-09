e6026cb00f80885dedcafa65d8840451ad0cf372
sfw <fuwens2019@163.com>
2019-11-09 11:26:02 +0800

    show //indirect in go.mod (#16)

--- eModule/cmd/module.go
+++ eModule/cmd/module.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"github.com/fuwensun/goms/pkg/hello"
-	"github.com/fuwensun/goms/pkg/world"
+	"github.com/gomsx/helloworld"
 )
 
 func module() {
-	var h hello.Hello
-	h.Print()
-	var w world.World
-	w.Print()
+	var hw helloworld.HelloWorld
+	hw.Print()
 }
