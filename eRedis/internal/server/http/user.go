f6b258e3922629c76a4426c743c37f2ac55795b9
sfw <fuwens2019@163.com>
2020-02-29 17:55:44 +0800

    fix typo

--- eRedis/internal/server/http/user.go
+++ eRedis/internal/server/http/user.go
@@ -17,14 +17,14 @@ func createUser(c *gin.Context) {
 
 	user.Sex, err = strconv.ParseInt(sexstr, 10, 64)
 	if sexstr == "" || err != nil {
-		log.Printf("sex err:%v\n", sexstr)
+		log.Printf("http sex err:%v\n", sexstr)
 		c.JSON(404, gin.H{"error": "uid err!"})
 		return
 	}
 	user.Name = namestr
 
 	if err = svc.CreateUser(c, &user); err != nil {
-		log.Printf("create user, err: %v", err)
+		log.Printf("http create user, err: %v", err)
 		c.JSON(404, gin.H{"error": "create failed!"})
 		return
 	}
@@ -46,20 +46,20 @@ func updateUser(c *gin.Context) {
 
 	user.Uid, err = strconv.ParseInt(uidstr, 10, 64)
 	if uidstr == "" || err != nil {
-		log.Printf("uid err:%v\n", uidstr)
+		log.Printf("http uid err:%v\n", uidstr)
 		c.JSON(404, gin.H{"error": "uid err!"})
 		return
 	}
 	user.Sex, err = strconv.ParseInt(sexstr, 10, 64)
 	if sexstr == "" || err != nil {
-		log.Printf("sex err:%v\n", sexstr)
+		log.Printf("http sex err:%v\n", sexstr)
 		c.JSON(404, gin.H{"error": "sex err!"})
 		return
 	}
 	user.Name = namestr
 	err = svc.UpdateUser(c, &user)
 	if err != nil {
-		log.Printf("update user,err: %v\n", err)
+		log.Printf("http update user,err: %v\n", err)
 		c.JSON(404, gin.H{"error": "data not found!"})
 		return
 	}
@@ -75,13 +75,13 @@ func readUser(c *gin.Context) {
 	uidstr := c.Query("uid")
 	uid, err := strconv.ParseInt(uidstr, 10, 64)
 	if uidstr == "" || err != nil {
-		log.Printf("uid err:%v\n", uidstr)
+		log.Printf("http uid err:%v\n", uidstr)
 		c.JSON(404, gin.H{"error": "uid err!"})
 		return
 	}
 	user, err := svc.ReadUser(c, uid)
 	if err != nil {
-		log.Printf("read user,err: %v\n", err)
+		log.Printf("http read user,err: %v\n", err)
 		c.JSON(404, gin.H{"error": "data not found!"})
 		return
 	}
@@ -97,12 +97,12 @@ func deleteUser(c *gin.Context) {
 	uidstr := c.Query("uid")
 	uid, err := strconv.ParseInt(uidstr, 10, 64)
 	if uidstr == "" || err != nil {
-		log.Printf("uid err:%v\n", uidstr)
+		log.Printf("http uid err:%v\n", uidstr)
 		c.JSON(404, gin.H{"error": "uid err!"})
 		return
 	}
 	if err = svc.DeleteUser(c, uid); err != nil {
-		log.Printf("delete user,err: %v\n", err)
+		log.Printf("http delete user,err: %v\n", err)
 		c.JSON(404, gin.H{"error": "data not found!"})
 		return
 	}
