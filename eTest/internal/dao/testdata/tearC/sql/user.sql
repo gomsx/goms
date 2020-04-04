2833f8ed627f7b4c326122a97bbfd4a8ee0243f3
sfw <fuwens2019@163.com>
2020-04-04 22:02:34 +0800

    fix eTest/.../dao/up_docker.sh

--- eTest/internal/dao/testdata/tearC/sql/user.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER 'utest'@'%' IDENTIFIED BY 'pwtest';
-grant all privileges on *.* to 'utest'@'%' with grant option;
-flush privileges;
-SELECT user,authentication_string,host FROM mysql.user;
-create database dbtest;
-use dbtest;
-show tables;
--- eTest/internal/dao/testdata/teardocker/up_docker.sh
+++ eTest/internal/dao/testdata/teardocker/up_docker.sh
@@ -6,13 +6,14 @@ set -x
 PWD=$(cd "$(dirname "$0")";pwd)
 echo $PWD
 
-# bash $PWD/down_docker.sh >&1 | tee $PWD/output.log
+bash $PWD/down_docker.sh >&1 | tee $PWD/output.log
 bash $PWD/up_mysql_docker.sh >&1 | tee -a $PWD/output.log
 bash $PWD/up_redis_docker.sh >&1 | tee -a $PWD/output.log
 
-set +x;echo " --------------- docker running ----------------";set -x
+echo -e "\n"
+set +x;echo "=== === === === === docker running === === === === === ";set -x
 
 docker ps | grep mysqltest
 docker ps | grep redistest
 
-echo -e "\n\n"
+echo -e "\n"
