45a8aa77587c4719e69f96345741cb68ff095f66
sfw <fuwens2019@163.com>
2020-04-07 19:07:17 +0800

    update doc

--- tool/test/bash/test.sh
+++ tool/test/bash/test.sh
@@ -5,13 +5,15 @@ ADDR=192.168.43.201:8080
 
 while true
 do
-	# 使用 http 方法 GET /ping
+	# ping
+	# GET /ping
 	curl -X GET $ADDR/ping -w "\n"
 
-	# 使用 http 方法 GET /ping, 参数 message=xxx
+	# GET /ping
 	curl -X GET $ADDR/ping?message=xxx -w "\n"
 	
-	# 使用 http 方法 POST /user/user, 参数 name=xxx sex=0
+	# user
+	# POST /user/user
 	curl -X POST -d "name=xxx&sex=1" $ADDR/user -w "\n"
 
 	res=$(curl -X POST -d "name=xxx&sex=1" 192.168.43.201:8080/user);#echo $res;
@@ -20,13 +22,13 @@ do
 	uid=$res;
 	name=name${uid:0:5};#echo $name
 
-	# 使用 http 方法 PUT /user/user, 参数 uid=123 name=yyy sex=1
-	curl -X PUT -d "name=$name&sex=1" $ADDR/user/$uid -w "\n"
+	# PUT /user/user
+	curl -X PUT -d "name=$name & sex=1" $ADDR/user/$uid -w "\n"
 
-	# 使用 http 方法 GET /user/user, 参数 uid=123
+	# GET /user/user
 	curl -X GET $ADDR/user/$uid -w "\n"
-	# curl -X GET $ADDR/user?uid=$uid -w "\n"
+	curl -X GET $ADDR/user?uid=$uid -w "\n"
 
-	# 使用 http 方法 DELETE /user/user, 参数 uid=123
+	# DELETE /user/user
 	curl -X DELETE $ADDR/user/$uid -w "\n"
 done
