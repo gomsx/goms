2d86968a31416be3d3c58fbcb2e48d61d6b691bf
sfw <fuwens2019@163.com>
2020-05-11 13:00:44 +0800

    add k8s install

--- tool/env/k8s_cluster/k8s-master-install.sh
+++ tool/env/k8s_cluster/k8s-master-install.sh
@@ -4,11 +4,23 @@ set -x
 set -e
 set -u
 
-PWD=$(cd "$(dirname "$0")";pwd)
-echo $PWD
+# PWD=$(cd "$(dirname "$0")";pwd)
+# echo $PWD
 
-DK=$PWD/../docker
-K8S=$PWD
+# DK=$PWD/../docker
+# K8S=$PWD
 
-$DK/install_docker.sh
-$DK/config_docker.sh
+# $DK/install_docker.sh
+# $DK/config_docker.sh
+
+# env
+./env/k8s_env_check.sh
+./env/k8s_env_install.sh
+./env/k8s_env_config.sh
+./env/k8s_env_check.sh
+
+# kubeadm
+./kubeadm/master/kubeadm_install_master.sh
+./kubeadm/master/kubeadm_init_master.sh
+./kubeadm/master/kubeadm_set_master.sh
+./kubeadm/master/kubeadm_check.sh
--- tool/env/k8s_cluster/k8s-node-install.sh
+++ tool/env/k8s_cluster/k8s-node-install.sh
@@ -4,14 +4,23 @@ set -x
 set -e
 set -u
 
-PWD=$(cd "$(dirname "$0")";pwd)
-echo $PWD
+# PWD=$(cd "$(dirname "$0")";pwd)
+# echo $PWD
 
-DK=$PWD/../docker
-K8S=$PWD
+# DK=$PWD/../docker
+# K8S=$PWD
 
-$DK/install_docker.sh
-$DK/config_docker.sh
+# $DK/install_docker.sh
+# $DK/config_docker.sh
+
+# env
+./env/k8s_env_check.sh
+./env/k8s_env_install.sh
+./env/k8s_env_config.sh
+./env/k8s_env_check.sh
+
+# kubeadm
+./kubeadm/node/kubeadm_install_node.sh
 
 
 
