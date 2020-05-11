0c2f4be290cfb6f029f89a3b50b0549713416897
sfw <fuwens2019@163.com>
2020-05-11 09:55:59 +0800

    update tool/env/k8s

--- tool/env/k8s_cluster/k8s_env_check.sh
+++ tool/env/k8s_cluster/k8s_env_check.sh
@@ -4,23 +4,26 @@
 # set -e
 # set -u
 
-# Selinux
+succ="[succ]"
+failed="[failed]"
+
+# 关闭 selinux
 # https://blog.csdn.net/wx740851326/article/details/72302931
 res=$(getenforce)
 echo "==> selinux $res"
-[[ $res = "Disabled" ]] && echo "[succ!]" || echo "[failed!]"
+[[ $res = "Disabled" ]] && echo $succ || echo $failed
 
-# swap
+# 关闭 swap
 # https://blog.csdn.net/ygm_linux/article/details/24532809
 res=$(free xargs | awk 'NR==3{ print $2 $3 $4 }')
 echo "==> swap $res"
-[[ $res = "000" ]] && echo "[succ!]" || echo "[failed!]"
+[[ $res = "000" ]] && echo $succ || echo $failed
 
-# ufw防火墙
+# 关闭 ufw 防火墙
 # https://blog.csdn.net/liukuan73/article/details/83116271
 res=$(sudo ufw status)
 #去空格
 res="$(echo -e "${res}" | tr -d '[:space:]')"
 res=${res##*Status:}
 echo "==> ufw $res"
-[[ $res = "inactive" ]] && echo "[succ!]" || echo "[failed!]"
+[[ $res = "inactive" ]] && echo $succ || echo $failed
--- tool/env/k8s_cluster/kubeadm/kubeadm_config_cluster.sh
+++ tool/env/k8s_cluster/kubeadm/kubeadm_config_cluster.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 set -xe
 
+
+# 关闭 selinux
+# https://segmentfault.com/q/1010000018470347
+# setenforce 0
+# sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
+
+# 关闭 swap
 # https://blog.csdn.net/CSDN_duomaomao/article/details/75142769
 sudo swapoff -a
 sudo mount -n -o remount,rw /
+
+# 关闭 ufw 防火墙
+# https://blog.csdn.net/liukuan73/article/details/83116271
+sudo ufw disable
 
