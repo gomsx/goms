8f824743ca963fcd7684f2939f1f42c110623878
sfw <fuwens2019@163.com>
2020-05-13 14:20:25 +0800

    clear tool/k8s

--- tool/env/k8s/plugin/flannel/get_flannel.sh
+++ tool/env/k8s/plugin/flannel/get_flannel.sh
@@ -5,5 +5,3 @@ sudo sysctl net.bridge.bridge-nf-call-iptables=1
 
 wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml  
 sed 's/quay.io\/coreos/registry.cn-beijing.aliyuncs.com\/imcto/g'
-
-sudo kubectl apply -f kube-flannel.yml
