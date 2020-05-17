cfb10e3129403d2ad492782283159c4661fc625a
sfw <fuwens2019@163.com>
2020-05-17 09:49:45 +0800

    add grpc tset for user to tool

--- tool/test/bash/test_grpc.sh
+++ tool/test/bash/test_grpc.sh
@@ -4,9 +4,28 @@ set -x
 ADDR=192.168.43.201:50051
 	
 # ping
-# GET /ping
+# Ping
 grpcurl -plaintext $ADDR service.goms.User/Ping 
 
-# GET /ping
+# Ping
 grpcurl -plaintext -d '{"Message": "xxx"}' $ADDR service.goms.User/Ping 
-	
+
+# user
+# CreateUser
+res=$(grpcurl -plaintext -d '{"Name": "xxx","Sex":"0"}' $ADDR service.goms.User/CreateUser)
+res=$(echo $res | awk 'NR==1{ print $3 }' | tr -d "\"")
+uid=$res;
+name=name${uid:1:6};echo $name
+
+# UpdateUser 
+data='{"Uid":"uid","Name":"name","Sex":"1"}'
+data=$(echo $data | sed s/uid/$uid/ |sed s/name/$name/)
+grpcurl -plaintext -d $data $ADDR service.goms.User/UpdateUser
+
+# ReadUser
+data='{"Val":"uid"}'
+data=$(echo $data | sed s/uid/$uid/)
+grpcurl -plaintext -d $data $ADDR service.goms.User/ReadUser
+
+# DeleteUser
+grpcurl -plaintext -d $data $ADDR service.goms.User/DeleteUser
