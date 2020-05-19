2dbb3581525e2016e2e1670e987a1cce621e0135
sfw <fuwens2019@163.com>
2020-05-19 15:26:53 +0800

    improve testing bash by pass ip and port

--- tool/test/bash/test_http.sh
+++ tool/test/bash/test_http.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -x
 
-ADDR=192.168.43.201:8080
+[ $1 ] && IP=$1 || IP=192.168.43.201
+[ $2 ] && PORT=$2 || PORT=8080
+
+ADDR="$IP:$PORT"
 
 # ping
 # GET /ping
@@ -14,11 +17,11 @@ curl -X GET $ADDR/ping?message=xxx -w "\n"
 # POST /user/user
 # curl -X POST -d "name=xxx&sex=1" $ADDR/user -w "\n"
 
-res=$(curl -X POST -d "name=xxx&sex=1" $ADDR/user);#echo $res;
-res=${res##*\"uid\":};#echo $res;
-res=${res%%\}*};#echo $res;
+res=$(curl -X POST -d "name=xxx&sex=1" $ADDR/user);     #echo $res;
+res=${res##*\"uid\":};  #echo $res;
+res=${res%%\}*};        #echo $res;
 uid=$res;
-name=name${uid:0:5};#echo $name
+name=name${uid:0:5};    #echo $name
 
 # PUT /user/user
 curl -X PUT -d "name=$name&sex=1" $ADDR/user/$uid -w "\n"
