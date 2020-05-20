a08782c6bf3f642a728c849cd47f4adaa811d4d5
sfw <fuwens2019@163.com>
2020-05-20 18:46:31 +0800

    improve ut for grpc ping in eTest

--- eTest/internal/server/grpc/ping_test.go
+++ eTest/internal/server/grpc/ping_test.go
@@ -2,7 +2,6 @@ package grpc
 
 import (
 	"context"
-	"errors"
 	"testing"
 
 	"github.com/fuwensun/goms/eTest/api"
@@ -13,14 +12,13 @@ import (
 )
 
 func TestPing(t *testing.T) {
-
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	svcm := mock.NewMockSvc(ctrl)
 	srv := Server{svc: svcm}
+	var ctx = context.Background()
 
 	Convey("TestPing should succ", t, func() {
-
 		//mock
 		var pc PingCount = 2
 		svcm.EXPECT().
@@ -28,35 +26,28 @@ func TestPing(t *testing.T) {
 			Return(pc, nil)
 
 		//构建 req
-		var ctx = context.Background()
 		req := &api.Request{
 			Message: "xxx",
 		}
 		//发起 req
-		resp, _ := srv.Ping(ctx, req)
-
+		resp, err := srv.Ping(ctx, req)
 		//断言
+		So(err, ShouldEqual, nil)
 		So(resp.Message, ShouldEqual, "Pong xxx")
 		So(resp.Count, ShouldEqual, pc)
 	})
 
 	Convey("TestPing should failed", t, func() {
-
 		//mock
-		err := errors.New("error")
 		svcm.EXPECT().
 			HandPingGrpc(gomock.Any()).
-			Return(PingCount(0), err)
+			Return(PingCount(0), ErrInternalError)
 
 		//构建 req
-		var ctx = context.Background()
-		req := &api.Request{
-			Message: "xxx",
-		}
+		req := &api.Request{}
 		//发起 req
-		resp, _ := srv.Ping(ctx, req)
-
+		_, err := srv.Ping(ctx, req)
 		//断言
-		So(resp.Message, ShouldEqual, "internal error!")
+		So(err, ShouldEqual, ErrInternalError)
 	})
 }
