d0bc93c85a8fcec574e97981d0b974218531a7bf
sfw <sfuwen@sina.com>
2020-05-25 19:55:11 +0800

    ==> clean tool/test

--- tool/test/bash/test_http.sh
+++ tool/test/bash/test_http.sh
@@ -15,13 +15,11 @@ curl -X GET $ADDR/ping?message=xxx -w "\n"
 	
 # user
 # POST /user/user
-# curl -X POST -d "name=xxx&sex=1" $ADDR/user -w "\n"
-
-res=$(curl -X POST -d "name=xxx&sex=1" $ADDR/user);     #echo $res;
-res=${res##*\"uid\":};  #echo $res;
-res=${res%%\}*};        #echo $res;
+res=$(curl -X POST -d "name=xxx&sex=1" $ADDR/user); 
+res=${res##*\"uid\":};  
+res=${res%%\}*};        
 uid=$res;
-name=name${uid:0:5};    #echo $name
+name=name${uid:0:5};    
 
 # PUT /user/user
 curl -X PUT -d "name=$name&sex=1" $ADDR/user/$uid -w "\n"
