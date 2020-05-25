db3ea262af582228b355628a3e68336ed718f609
sfw <sfuwen@sina.com>
2020-05-25 20:29:11 +0800

    ==> rm tool/sql

--- tool/sql/init_mysql.sql
+++
@@ -1,9 +0,0 @@
-use mysql;
-CREATE USER 'utest'@'%' IDENTIFIED BY 'pwtest';
-grant all privileges on *.* to 'utest'@'%' with grant option;
-flush privileges;
-SELECT user,authentication_string,host FROM mysql.user;
-
-create database dbtest;
-dbtest < /sql/dbtest.sql;
-use dbtest;show tables;
