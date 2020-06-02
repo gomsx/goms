48fa9d6100652861d51d98b5a899d110ab275163
sfw <sfuwen@sina.com>
2020-06-03 05:49:27 +0800

    add log for UpdateUser() of dao

--- eApi/internal/dao/user.go
+++ eApi/internal/dao/user.go
@@ -169,7 +169,7 @@ func (d *dao) CreateUser(c context.Context, user *User) error {
 	return nil
 }
 
-// Cache Aside 写策略
+// Cache Aside 写策略,更新
 func (d *dao) UpdateUser(c context.Context, user *User) error {
 	// 先更新 DB
 	if err := d.UpdateUserDB(c, user); err != nil {
@@ -178,6 +178,8 @@ func (d *dao) UpdateUser(c context.Context, user *User) error {
 	}
 	// 再删除 cache
 	if err := d.DelUserCC(c, user.Uid); err != nil {
+		// DB，cache 数据不一致
+		log.Error().Msgf("delete user cache error after update db, uid=%v, err=%v", user.Uid, err)
 		err = fmt.Errorf("delete user in cc: %w", err)
 		return err
 	}
@@ -216,11 +218,14 @@ func (d *dao) ReadUser(c context.Context, uid int64) (*User, error) {
 	return user, nil
 }
 
+// Cache Aside 写策略,删除
 func (d *dao) DeleteUser(c context.Context, uid int64) error {
+	// 先删除 DB
 	if err := d.DeleteUserDB(c, uid); err != nil {
 		err = fmt.Errorf("del user in db: %w", err)
 		return err
 	}
+	// 再删除 cache
 	if err := d.DelUserCC(c, uid); err != nil {
 		err = fmt.Errorf("del user in cc: %w", err)
 		return err
