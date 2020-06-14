e3557325d3048f898fb9eae07afbd8dc96027f7c
sfw <sfuwen@sina.com>
2020-06-14 18:17:25 +0800

    fix test bash

--- tool/test/bash/testgrpc.sh
+++ tool/test/bash/testgrpc.sh
@@ -4,8 +4,11 @@ set -x
 PWD=$(cd "$(dirname "$0")";pwd)
 echo $PWD
 
+echo "$1"
+echo "$2"
+
 while true
 do
-	bash $PWD/test_grpc.sh $1 $2 $3
+	bash $PWD/test_grpc.sh "$1" "$2" "$3"
 	sleep 1
 done
--- tool/test/bash/testhttp.sh
+++ tool/test/bash/testhttp.sh
@@ -6,6 +6,7 @@ echo $PWD
 
 while true
 do
-	bash $PWD/test_http.sh $1 $2 $3
+	# bash $PWD/test_http.sh $1 $2 $3			# 如果 $1 为 ""(即空),那么 test_http.sh 无法获取到
+	bash $PWD/test_http.sh "$1" "$2" "$3"		# 如果 $1 为 ""(即空),那么 test_http.sh 可以获取到 
 	sleep 1
 done
