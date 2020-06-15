ecf7dc0efc7a47afd68d70879adc1f16067ed1dd
sfw <sfuwen@sina.com>
2020-06-16 06:41:12 +0800

    update local env

--- tool/env/local/env_install.sh
+++ tool/env/local/env_install.sh
@@ -5,14 +5,25 @@ set -u
 sudo apt install mysql-server
 sudo apt install redis-server
 
-#curl
+# curl
 sudo apt install curl
-#grpcurl
+
+# grpcurl
 go get -u github.com/fullstorydev/grpcurl
 go install github.com/fullstorydev/grpcurl/cmd/grpcurl
-#mockgen
+
+# mockgen
 go get -u github.com/golang/mock/mockgen
 go install github.com/golang/mock/mockgen
-#protoc
+
+# protoc
+sudo apt install protobuf-compiler
 go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
 go install github.com/golang/protobuf/protoc-gen-go
+
+# grpc
+go get -u google.golang.org/grpc
+
+# grpc-gateway
+go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
+go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
