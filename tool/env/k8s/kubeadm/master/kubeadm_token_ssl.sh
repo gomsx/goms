e4914599fd6dcb6c9b3779c60b9b32b5fa853067
vuca <aivuca@163.com>
2020-07-03 16:34:16 +0800

    update k8s

--- tool/env/k8s/kubeadm/master/kubeadm_token_ssl.sh
+++ tool/env/k8s/kubeadm/master/kubeadm_token_ssl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-set -xe
+# set -x
+set -e
 
 kubeadm token create
 openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //'
