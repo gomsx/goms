5596dea4aff941a8a5404bcfa7a408c9e2cc367e
vuca <aivuca@163.com>
2020-07-05 20:31:08 +0800

    add check changed doc to tool/check

--- tool/code/check.sh
+++ tool/code/check.sh
@@ -5,39 +5,12 @@ set -x
 PWD=$(cd "$(dirname "$0")";pwd)
 echo $PWD
 
-# PRO
-PRO=$PWD/../..
-
 # chmod +x
 find $PRO -name "*.sh" | xargs chmod +x 
 
-# go fmt
-go fmt $PRO/...
+# doc
+$PWD/ck_doc_deta.sh
 
-# 逃逸分析，'-l' 防止内联
-# go build -gcflags '-m -l'
+# code
+$PWD/ck_code_go.sh
 
-###############################
-# PWD 
-PWD=$(cd "$(dirname "$0")";pwd)
-# PRO
-PRO=$PWD/../..
-PRO=$(cd $PRO;pwd)
-BN=$(basename $PWD)
-# CMD
-CMD="find $PRO -name \"*\" -type f | grep -v .git | grep -v $BN" 
-# FILES
-FILES=$(eval $CMD)
-echo "==> FILES: $FILES"
-
-for f in $FILES
-do
-# 删除首行的空行
-sed -i '/./,$!d' $f
-# 匹配空格、tab等特殊字符
-sed -i 's/^\s*$/\n/g' $f
-# 尾行部插入空行
-sed -i '$a\\n' $f
-# 合并多个空行
-sed -i '/^$/{N;/^\n*$/D}' $f
-done
