235d9b17613622489fd854217f1a35593c5df0b9
vuca <aivuca@163.com>
2020-07-05 14:59:32 +0800

    fix flannel config bash for k8s

--- tool/env/k8s/plugin/flannel/config_flannel.sh
+++ tool/env/k8s/plugin/flannel/config_flannel.sh
@@ -7,7 +7,8 @@
 rm -rf /etc/cni/net.d/
 mkdir -p /etc/cni/net.d/
 
-cat <<EOF> /etc/cni/net.d/12-flannel.conf
+# 10-flannel.conf 名中 10 和 flannel 版本无关，0.8 版中也是就是这名
+cat <<EOF> /etc/cni/net.d/10-flannel.conf
 {"name":"cbr0","type":"flannel","delegate": {"isDefaultGateway": true}}
 EOF
 
@@ -19,17 +20,18 @@ mkdir /run/flannel/
 touch /run/flannel/subnet.env
 chmod a+rwx /run/flannel/subnet.env
 
-# cat <<EOF> /run/flannel/subnet.env
-# FLANNEL_NETWORK=10.1.0.0/16
-# FLANNEL_SUBNET=10.1.17.1/24
-# FLANNEL_MTU=1472
-# FLANNEL_IPMASQ=true
-# EOF
-
+# ok
 cat <<EOF> /run/flannel/subnet.env
-FLANNEL_NETWORK=172.100.0.0/16
-FLANNEL_SUBNET=172.100.1.0/24
-FLANNEL_MTU=1450
+FLANNEL_NETWORK=10.1.0.0/16
+FLANNEL_SUBNET=10.1.17.1/24
+FLANNEL_MTU=1472
 FLANNEL_IPMASQ=true
 EOF
+
+# cat <<EOF> /run/flannel/subnet.env
+# FLANNEL_NETWORK=172.100.0.0/16
+# FLANNEL_SUBNET=172.100.1.0/24
+# FLANNEL_MTU=1450
+# FLANNEL_IPMASQ=true
+# EOF
 
