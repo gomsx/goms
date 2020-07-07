0d37d5ba40f2cdacf51546d4f97c03ab718018c2
vuca <aivuca@163.com>
2020-07-07 20:01:06 +0800

    new istio env

--- tool/env/istio/istio-install.sh
+++ tool/env/istio/istio-install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
 set -x
 
+#
 sudo tar -C /usr/local -xvzf istio-1.6.4-linux-amd64.tar.gz 
 echo "PATH=/usr/local/istio-1.6.4:\$PATH" >> .bashrc
 source .bashrc 
+
+#
+istioctl profile list
 istioctl manifest apply --set profile=demo
 
