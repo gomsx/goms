abc342640d8e45b8d23896cb2afdd501efbaed18
sfw <sfuwen@sina.com>
2020-07-10 08:06:18 +0800

    update docker env

--- tool/env/docker/install_docker.sh
+++ tool/env/docker/install_docker.sh
@@ -6,6 +6,8 @@ set -x
 set -e
 set -u
 
+DOCKER_VERSION=5:19.03.8~3-0
+
 sudo apt-get remove docker docker-engine docker-ce docker.io -y
 sudo apt-get update -y
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common  -y
@@ -15,6 +17,6 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update -y
 
 apt-cache madison docker-ce
-sudo apt-get install docker-ce=5:18.09.0~3-0~ubuntu-bionic -y
+sudo apt-get install docker-ce=$DOCKER_VERSION~ubuntu-bionic -y
 systemctl status docker
 
