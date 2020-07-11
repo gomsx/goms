5ad74be826c32e8f09e7b80fa1e12c2cdd4dca3f
vuca <aivuca@163.com>
2020-07-11 10:22:31 +0800

    update tools/env

---
+++ tools/env/k8sx/proxy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://www.liuyixiang.com/post/108262.html
+# option2：kube-proxy 端口转发
+kubectl proxy --address='100.64.198.131' --port=9090 --accept-hosts='^*$'
+
+#URL：http://100.64.198.131:9090/api/v1/namespaces/istio-system/services/http:prometheus:9090/proxy
