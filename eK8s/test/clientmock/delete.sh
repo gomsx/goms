3fbd146001540e8651318031441e08f951fb2391
vuca <aivuca@163.com>
2020-07-11 21:11:02 +0800

    rename clientmock to clienttest

--- eK8s/test/clientmock/delete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# set -xe
-
-PWD=$(cd "$(dirname "$0")";pwd)
-echo $PWD
-
-kubectl delete -f $PWD/clientmock-deploy.yaml --namespace="$1"
-kubectl delete -f $PWD/clientmock-svc.yaml --namespace="$1"
-
