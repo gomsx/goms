1072ea54b68b596c9b8bceb375db229147432251
sfw <sfuwen@sina.com>
2020-07-15 17:26:45 +0800

    new k8sx to env

---
+++ tools/env/k8sx/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -x
+
+git clone https://github.com/ahmetb/kubectx /opt/kubectx
+ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
