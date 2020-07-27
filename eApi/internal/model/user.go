63df3117f082a5cc33d1b50ad47f363e54ea472c
vuca <aivuca@163.com>
2020-07-27 10:19:44 +0800

    fix sex val to [1,2] for eApi

--- eApi/internal/model/user.go
+++ eApi/internal/model/user.go
@@ -17,7 +17,7 @@ func init() {
 type User struct {
 	Uid  int64  `redis:"uid" validate:"required,gte=0"`
 	Name string `redis:"name" validate:"required,min=1,max=18"`
-	Sex  int64  `redis:"sex" validate:"required,gte=0,lte=1"`
+	Sex  int64  `redis:"sex" validate:"required,gte=1,lte=2"`
 }
 
 var UserErrMap = map[string]error{
