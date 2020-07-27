2840397fa6bd1a45cd84da68b3e737f4704c549b
sfw <sfuwen@sina.com>
2020-07-27 10:37:42 +0800

    update client docker version for tests

--- tests/bash/testhttp.sh
+++ tests/bash/testhttp.sh
@@ -6,7 +6,7 @@ echo $PWD
 
 while true
 do
-	# bash $PWD/test_http.sh $1 $2 $3			# 如果 $1 为 ""(即空),那么 test_http.sh 无法获取到
-	bash $PWD/test_http.sh "$1" "$2" "$3"		# 如果 $1 为 ""(即空),那么 test_http.sh 可以获取到 
+	# bash $PWD/test_http.sh $1 $2 $3 			# 如果 $1 为 ""(即空),那么 test_http.sh 无法获取到
+	bash $PWD/test_http.sh "$1" "$2" "$3" "$4"		# 如果 $1 为 ""(即空),那么 test_http.sh 可以获取到 
 done
 
