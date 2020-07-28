6f4c3e111ddbc8893d52338c89929fff9c71d94f
sfw <sfuwen@sina.com>
2020-07-28 09:13:59 +0800

    update argocd for tools/env

--- tools/env/argocd/deploy-argocd.sh
+++ tools/env/argocd/deploy-argocd.sh
@@ -6,7 +6,6 @@ wget https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install
 kubectl create namespace argocd
 kubectl apply -n argocd -f install.yaml
 
-
 # kubectl patch svc -n argocd argocd-server -p '{"spec": {"type": "NodePort","ports": [{"port":80,"nodePort":31140}]}}'
 kubectl patch svc -n argocd argocd-server -p '{"spec": {"type": "NodePort","ports": [{"port":80,"nodePort":31140},{"port":443,"nodePort":31141}]}}'
 
