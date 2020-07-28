81be551dc5d0b413f2d525d01fde26688b64c03c
sfw <sfuwen@sina.com>
2020-07-28 09:15:41 +0800

    update istio for tools/env

--- tools/env/istio/install-istio.sh
+++ tools/env/istio/install-istio.sh
@@ -3,7 +3,7 @@ set -x
 
 #
 sudo tar -C /usr/local -xvzf istio-1.6.4-linux-amd64.tar.gz 
-echo "PATH=/usr/local/istio-1.6.4:\$PATH" >> .bashrc
+echo "PATH=/usr/local/istio-1.6.4/bin:\$PATH" >> .bashrc
 source .bashrc 
 
 #
