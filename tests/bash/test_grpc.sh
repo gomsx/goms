242ea6b09e7587dc7f5559e222484cb6e3b0c1e1
sfw <sfuwen@sina.com>
2020-07-29 09:47:10 +0800

    fix bash for tests

--- tests/bash/test_grpc.sh
+++ tests/bash/test_grpc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-set -x
+# set -x
+set +x
 
 [ $1 ] && US=$1 || US=100
 [ $2 ] && SERVICE="service.goms.$2" ||SERVICE="service.goms" 
@@ -8,51 +9,49 @@ set -x
 
 ADDR="$HOST:$PORT"
 
-# usleep : 默认以微秒。  
-# 1s = 1000ms = 1000000us
 function delay(){
-    # sleep 1
-    usleep $US
+# set +x
+    for ((i=0;i<"$US";i="$i"+1))
+    do
+        # sleep 0.01
+        a=1
+    done
     echo "==> delay $US us"
+# set -x
 }
 
-# ping
+echo "-------------ping---------------"
 # Ping
 grpcurl -plaintext $ADDR $SERVICE.User/Ping 
-
 # Ping
 grpcurl -plaintext -d '{"message":"xxx"}' $ADDR $SERVICE.User/Ping 
 
-# user
+echo "-------------user---------------"
 # CreateUser
-data='{"name":"xxx","sex":"1"}'
-CMD="grpcurl -plaintext -d \$data \$ADDR \$SERVICE.User/CreateUser"
+DATA='{"name":"xxx","sex":"1"}'
+CMD="grpcurl -plaintext -d \$DATA \$ADDR \$SERVICE.User/CreateUser"
+RES=$(eval $CMD)
+delay
 
-res=$(eval $CMD)
+RES=$(echo $RES | awk 'NR==1{ print $3 }' | tr -d \"\"\")
+UIDX=$RES
 
-CMD="echo $res | awk 'NR==1{ print \$3 }' | tr -d \"\"\""
-res=$(eval $CMD)
-uid=$res
-name=name${uid:1:6}
-
+# ReadUser
+DATA='{"uid":"=uid"}'
+DATA=$(echo $DATA | sed s/=uid/$UIDX/)
+grpcurl -plaintext -d $DATA $ADDR $SERVICE.User/ReadUser
 delay
 
 # UpdateUser 
-data='{"uid":"=uid","name":"=name","sex":"1"}'
-data=$(echo $data | sed s/=uid/$uid/ |sed s/=name/$name/)
-grpcurl -plaintext -d $data $ADDR $SERVICE.User/UpdateUser
-
-delay
-
-# ReadUser
-data='{"uid":"=uid"}'
-data=$(echo $data | sed s/=uid/$uid/)
-grpcurl -plaintext -d $data $ADDR $SERVICE.User/ReadUser
-
+NAME=name${uid:1:6}
+DATA='{"uid":"=uid","name":"=name","sex":"1"}'
+DATA=$(echo $DATA | sed s/=uid/$UIDX/ |sed s/=name/$NAME/)
+grpcurl -plaintext -d $DATA $ADDR $SERVICE.User/UpdateUser
 delay
 
 # DeleteUser
-grpcurl -plaintext -d $data $ADDR $SERVICE.User/DeleteUser
-
+DATA='{"uid":"=uid"}'
+DATA=$(echo $DATA | sed s/=uid/$UIDX/)
+grpcurl -plaintext -d $DATA $ADDR $SERVICE.User/DeleteUser
 delay
 
