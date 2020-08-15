12812572dc763e5e5c840aca111b24871a8f2187
vuca <aivuca@163.com>
2020-08-15 09:24:29 +0800

    refactor(eTest/test):code style

--- eTest/internal/service/ping_test.go
+++ eTest/internal/service/ping_test.go
@@ -12,7 +12,6 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-//
 func TestHandPing(t *testing.T) {
 	Convey("TestHandPing", t, func() {
 		ctrl := gomock.NewController(t)
@@ -21,14 +20,9 @@ func TestHandPing(t *testing.T) {
 		svc := service{dao: daom}
 
 		Convey("for succ", func() {
-			p := &m.Ping{
-				Type:  "http",
-				Count: 2,
-			}
-			want := &m.Ping{
-				Type:  "http",
-				Count: 3,
-			}
+			p := &m.Ping{Type: "http", Count: 2}
+			want := &m.Ping{Type: "http", Count: 3}
+
 			daom.EXPECT().
 				ReadPing(gomock.Any(), p.Type).
 				Return(p, nil)
@@ -38,7 +32,7 @@ func TestHandPing(t *testing.T) {
 				Return(nil)
 
 			got, err := svc.HandPing(context.Background(), p)
-			So(reflect.DeepEqual(got, want), ShouldEqual, true)
+			So(reflect.DeepEqual(got, want), ShouldBeTrue)
 			So(err, ShouldBeNil)
 		})
 	})
