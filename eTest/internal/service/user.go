2d49fbf28b35770d59b3be9ec3d6d7fac091cf98
vuca <aivuca@163.com>
2020-08-15 15:20:16 +0800

    refactor(eTest/service):add Ctx to log

--- eTest/internal/service/user.go
+++ eTest/internal/service/user.go
@@ -12,7 +12,8 @@ import (
 func (s *service) CreateUser(c context.Context, user *m.User) error {
 	err := s.dao.CreateUser(c, user)
 	if err != nil {
-		log.Error().Msgf("failed to create user, err = %v", err)
+		log.Ctx(c).Error().
+			Msgf("failed to create user, err = %v", err)
 		return err
 	}
 	return nil
@@ -22,7 +23,8 @@ func (s *service) CreateUser(c context.Context, user *m.User) error {
 func (s *service) ReadUser(c context.Context, uid int64) (*m.User, error) {
 	user, err := s.dao.ReadUser(c, uid)
 	if err != nil {
-		log.Error().Msgf("failed to read user, err = %v", err)
+		log.Ctx(c).Error().
+			Msgf("failed to read user, err = %v", err)
 		return nil, err
 	}
 	return user, nil
@@ -32,7 +34,8 @@ func (s *service) ReadUser(c context.Context, uid int64) (*m.User, error) {
 func (s *service) UpdateUser(c context.Context, user *m.User) error {
 	err := s.dao.UpdateUser(c, user)
 	if err != nil {
-		log.Error().Msgf("failed to update user, err = %v", err)
+		log.Ctx(c).Error().
+			Msgf("failed to update user, err = %v", err)
 		return err
 	}
 	return nil
@@ -42,7 +45,8 @@ func (s *service) UpdateUser(c context.Context, user *m.User) error {
 func (s *service) DeleteUser(c context.Context, uid int64) error {
 	err := s.dao.DeleteUser(c, uid)
 	if err != nil {
-		log.Error().Msgf("failed to delete user, err = %v", err)
+		log.Ctx(c).Error().
+			Msgf("failed to delete user, err = %v", err)
 		return err
 	}
 	return nil
