1d95e9a2802ef613d8b094c9fc53e6f3f897befa
sfw <sfuwen@sina.com>
2020-08-16 10:10:24 +0800

    fix create user sql for etest/setup/msyql

--- tests/setup/mysql/user/create.sql
+++ tests/setup/mysql/user/create.sql
@@ -1,7 +1,7 @@
 -- 创建用户
 use mysql;
 CREATE USER 'utest'@'%' IDENTIFIED BY 'pwtest';
-grant all privileges on *.* to 'utest'@'%' IDENTIFIED BY 'pwtest' with grant option;
+grant all privileges on *.* to 'utest'@'%';
 flush privileges;
 SELECT user,authentication_string,host FROM mysql.user;
 
