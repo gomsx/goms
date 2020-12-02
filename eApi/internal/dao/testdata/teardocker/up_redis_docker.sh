56f50fe0b005f084e3bc8b5eebd82fe878b9abd2
sfw <sfuwen@sina.com>
2020-12-02 14:02:27 +0800

    style eApi/dao

--- eApi/internal/dao/testdata/teardocker/up_redis_docker.sh
+++ eApi/internal/dao/testdata/teardocker/up_redis_docker.sh
@@ -7,7 +7,11 @@ echo "=================== up_redis_docker ========================"
 set -x
 
 #run docker
-docker run --name redistest -p 26379:6379 -d redistest
+docker run \
+    --name redistest \
+    -p 26379:6379 \
+    -d \
+    redistest
 
 #ps docker
 docker ps | grep redistest
