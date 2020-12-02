dd1550c892f85112168c4a6eea11b60a6446fbb2
vuca <aivuca@163.com>
2020-12-02 12:18:02 +0800

    test(eApi/dao):fix control-docker-bash for ut

--- eApi/internal/dao/testdata/teardocker/down_docker.sh
+++ eApi/internal/dao/testdata/teardocker/down_docker.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -x
-# set -e
+set -e
 
-# .sh所在目录
+## .sh所在目录
 PWD=$(cd "$(dirname "$0")";pwd)
 echo $PWD
 
-#
+## bash
 bash $PWD/down_mysql_docker.sh
 bash $PWD/down_redis_docker.sh
+
+## echo
+set +x
+echo "==================== docker stop ======================"
+set -x
 
--- eApi/internal/dao/testdata/teardocker/up_docker.sh
+++ eApi/internal/dao/testdata/teardocker/up_docker.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 set -x
-# set -e
+set -e
 
 ## .sh所在目录
 PWD=$(cd "$(dirname "$0")";pwd)
 echo $PWD
 
+## bash
 bash $PWD/down_docker.sh >&1 | tee $PWD/output.log
 bash $PWD/up_mysql_docker.sh >&1 | tee -a $PWD/output.log
 bash $PWD/up_redis_docker.sh >&1 | tee -a $PWD/output.log
 
-echo -e "\n"
-set +x;echo "=== === === === === docker running === === === === === ";set -x
-
-docker ps | grep mysqltest
-docker ps | grep redistest
-
-echo -e "\n"
+## echo
+set +x
+echo "==================== docker running ======================"
+set -x
 
