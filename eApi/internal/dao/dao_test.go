a31809a9ceb4c366eb54c9b7295fde1e26ca7cda
sfw <sfuwen@sina.com>
2020-12-03 23:16:09 +0800

    fix docker bash for ut of eApi

--- eApi/internal/dao/dao_test.go
+++ eApi/internal/dao/dao_test.go
@@ -50,15 +50,15 @@ func tearupDocker() {
 	if !isCiEnvDocker() {
 		return
 	}
-	//
-	command := "./testdata/teardocker/up_docker.sh" // command := "ls -al"
+	// bash command
+	command := "./testdata/teardocker/up_docker.sh"
 	cmd := exec.Command("/bin/bash", "-c", command)
 	output, err := cmd.Output()
 	if err != nil {
-		fmt.Printf("Execute Shell: %s failed with error: %s\n", command, err.Error())
-		// return
+		fmt.Printf("Execute Shell: [%s] failed with error: %s\n", command, err.Error())
+		return
 	}
-	fmt.Printf("Execute Shell: %s finished with output:\n%s\n", command, string(output))
+	fmt.Printf("Execute Shell: [%s] succ to finished with output:\n%s\n", command, string(output))
 	//等待 mysql docker 初始化完成
 	time.Sleep(time.Second * 25)
 }
@@ -71,8 +71,8 @@ func teardownDocker() {
 	cmd := exec.Command("/bin/bash", "-c", command)
 	output, err := cmd.Output()
 	if err != nil {
-		fmt.Printf("Execute Shell: %s failed with error: %s\n", command, err.Error())
-		// return
+		fmt.Printf("Execute Shell: [%s] failed with error: %s\n", command, err.Error())
+		return
 	}
-	fmt.Printf("Execute Shell: %s finished with output:\n%s\n", command, string(output))
+	fmt.Printf("Execute Shell: [%s] succ to finished with output:\n%s\n", command, string(output))
 }
--- eApi/internal/dao/testdata/teardocker/up_redis_docker.sh
+++ eApi/internal/dao/testdata/teardocker/up_redis_docker.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -x
-# set -e
+set -e
 
 set +x
 echo "=================== up_redis_docker ========================"
 set -x
 
-#run docker
+## run docker
 docker run \
     --name redistest \
     -p 26379:6379 \
     -d \
-    redistest
+    goms-redistest
 
-#ps docker
+## ps docker
 docker ps | grep redistest
 
