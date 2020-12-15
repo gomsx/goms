c5529c91290d04bcb022c310cb20968d8dee6c16
sfw <sfuwen@sina.com>
2020-12-15 09:29:15 +0800

    upgrade: upgrade protoc-gen-swagger plugin to protoc-gen-openapiv2

--- eApi/api/v1/pb/pb.go
+++ eApi/api/v1/pb/pb.go
@@ -1,6 +1,6 @@
 //go:generate protoc -I ./ -I ../../../../pkg/proto --go_out=plugins=grpc:../ api.proto
 //go:generate protoc -I ./ -I ../../../../pkg/proto --grpc-gateway_out=logtostderr=true:../ api.proto
-//go:generate protoc -I ./ -I ../../../../pkg/proto --swagger_out=logtostderr=true:../ api.proto
+//go:generate protoc -I ./ -I ../../../../pkg/proto --openapiv2_out=logtostderr=true:../ api.proto
 package pb
 
 // 生成的代码中 "google/api" 要用 "google.golang.org/genproto/googleapis/api/annotations" 替代
