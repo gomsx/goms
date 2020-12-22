9f13baf3cbcc7c941c50cfc7ad6fcee4b6862d84
vuca <aivuca@163.com>
2020-12-22 09:31:07 +0800

    chore(tools): fmt bash for release

--- tools/release/fix-branch.sh
+++ tools/release/fix-branch.sh
@@ -2,29 +2,37 @@
 # set -x
 set -e
 
-# VER 版本参数
-VER="$1"
+# ver 版本参数
+ver="$1"
 
 # 如果没有参数，打印提示并退出
-if [ -z $VER ];then
+if [ -z $ver ];then
     echo -e "❗ 错误，缺版本参数 \n格式: bash_cmd src_dir \n例子：fix-branch.sh v1.0.1"
     exit
 else
-    echo "版本参数 $VER"
+    echo "版本参数 $ver"
 fi
 
-# 当前 bash 所在目录路径 PWD
-PWD=$(cd "$(dirname "$0")";pwd)
+# 当前目录路径
+pwdx=$(
+	cd "$(dirname "$0")"
+	pwd
+)
 
-# 当前项目路径 PRO
-PRO=$PWD/../..
-PRO=$(cd $PRO;pwd)
+# 当前项目路径 pro
+pro=$(
+	cd "$pwdx/../.."
+	pwd
+)
+
+# workflow 目录
+wfd=$pro/workflows
 
 # 替换版本参数
-sed -i "s/_master/_$VER/g" $PRO/README.md
-sed -i "s/\/master/\/release-$VER/g" $PRO/README.md
+sed -i "s/_master/_$ver/g" $pro/README.md
+sed -i "s/\/master/\/release-$ver/g" $pro/README.md
 
-sed -i "s/_master/_$VER/g" $PRO/.github/workflows/make_master.yml
-sed -i "s/master/release-$VER/g" $PRO/.github/workflows/make_master.yml
-mv $PRO/.github/workflows/make_master.yml $PRO/.github/workflows/make_$VER.yml
+sed -i "s/_master/_$ver/g" $wfd/make_master.yml
+sed -i "s/master/release-$ver/g" $wfd/make_master.yml
+mv $wfd/make_master.yml $wfd/make_$ver.yml
 
