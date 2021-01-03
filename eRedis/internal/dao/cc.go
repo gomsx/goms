14aad227d1f635c1ca847eb567a08086a03d8e06
vuca <aivuca@163.com>
2021-01-03 21:35:21 +0800

    fix(eRedis): hand DSN/Addr-IS-EMPTY for file/env

--- eRedis/internal/dao/cc.go
+++ eRedis/internal/dao/cc.go
@@ -22,17 +22,24 @@ type cccfg struct {
 func getCCConfig(cfgpath string) (*cccfg, error) {
 	var err error
 	cfg := &cccfg{}
-
+	// file
 	path := filepath.Join(cfgpath, "redis.yaml")
-	if err = conf.GetConf(path, &cfg); err != nil { //file
+	if err = conf.GetConf(path, &cfg); err != nil {
 		log.Printf("get cc config file error: %v", err)
-	} else if cfg.Addr != "" {
+	} else if cfg.Addr == "" {
+		log.Printf("get db config file succ, but ADDR IS EMPTY")
+	} else {
 		log.Printf("get cc config file succ, Addr: %v", cfg.Addr)
 		return cfg, nil
-	} else if cfg.Addr = os.Getenv("REDIS_SVC_ADDR"); cfg.Addr == "" { //env
-		log.Printf("get cc config env error: %v", e.ErrNotFoundData)
+	}
+	// env
+	if addr, exist := os.LookupEnv("REDIS_SVC_ADDR"); exist == false {
+		log.Printf("get db config env error: %v", e.ErrNotFoundData)
+	} else if addr == "" {
+		log.Printf("get db config env succ, but ADDR IS EMPTY")
 	} else {
-		log.Printf("get cc config env succ, Addr: %v", cfg.Addr)
+		log.Printf("get db config env succ, Addr: %v", cfg.Addr)
+		cfg.Addr = addr
 		return cfg, nil
 	}
 	return nil, e.ErrNotFoundData
