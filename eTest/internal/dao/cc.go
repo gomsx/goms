b514093fff5b333cc7e68d0d1d42d277f6c1f765
sfw <sfuwen@sina.com>
2021-01-07 14:32:14 +0800

    fix: log msg

--- eTest/internal/dao/cc.go
+++ eTest/internal/dao/cc.go
@@ -27,18 +27,18 @@ func getCCConfig(cfgpath string) (*cccfg, error) {
 	if err = conf.GetConf(path, &cfg); err != nil {
 		log.Warn().Msgf("get cc config file error: %v", err)
 	} else if cfg.Addr == "" {
-		log.Warn().Msgf("get db config file succ, but ADDR IS EMPTY")
+		log.Warn().Msgf("get cc config file succ, but ADDR IS EMPTY")
 	} else {
 		log.Info().Msgf("get cc config file succ, Addr: %v", cfg.Addr)
 		return cfg, nil
 	}
 	// env
 	if addr, exist := os.LookupEnv("REDIS_SVC_ADDR"); exist == false {
-		log.Warn().Msgf("get db config env error: %v", e.ErrNotFoundData)
+		log.Warn().Msgf("get cc config env error: %v", e.ErrNotFoundData)
 	} else if addr == "" {
-		log.Warn().Msgf("get db config env succ, but ADDR IS EMPTY")
+		log.Warn().Msgf("get cc config env succ, but ADDR IS EMPTY")
 	} else {
-		log.Info().Msgf("get db config env succ, Addr: %v", cfg.Addr)
+		log.Info().Msgf("get cc config env succ, Addr: %v", cfg.Addr)
 		cfg.Addr = addr
 		return cfg, nil
 	}
