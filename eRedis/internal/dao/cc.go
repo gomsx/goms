b239ff689c41478de87d43778964c957265d268a
vuca <aivuca@163.com>
2021-01-07 14:09:35 +0800

    fix(all): log msg

--- eRedis/internal/dao/cc.go
+++ eRedis/internal/dao/cc.go
@@ -27,18 +27,18 @@ func getCCConfig(cfgpath string) (*cccfg, error) {
 	if err = conf.GetConf(path, &cfg); err != nil {
 		log.Printf("get cc config file error: %v", err)
 	} else if cfg.Addr == "" {
-		log.Printf("get db config file succ, but ADDR IS EMPTY")
+		log.Printf("get cc config file succ, but ADDR IS EMPTY")
 	} else {
 		log.Printf("get cc config file succ, Addr: %v", cfg.Addr)
 		return cfg, nil
 	}
 	// env
 	if addr, exist := os.LookupEnv("REDIS_SVC_ADDR"); exist == false {
-		log.Printf("get db config env error: %v", e.ErrNotFoundData)
+		log.Printf("get cc config env error: %v", e.ErrNotFoundData)
 	} else if addr == "" {
-		log.Printf("get db config env succ, but ADDR IS EMPTY")
+		log.Printf("get cc config env succ, but ADDR IS EMPTY")
 	} else {
-		log.Printf("get db config env succ, Addr: %v", cfg.Addr)
+		log.Printf("get cc config env succ, Addr: %v", cfg.Addr)
 		cfg.Addr = addr
 		return cfg, nil
 	}
