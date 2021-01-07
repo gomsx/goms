6a44b9560549dbd2f10838e9bf363d70ea57779c
vuca <aivuca@163.com>
2021-01-07 21:08:00 +0800

    refactor(all): clean

--- eApi/internal/dao/user_cc_test.go
+++ eApi/internal/dao/user_cc_test.go
@@ -147,24 +147,4 @@ func TestGetUserCC(t *testing.T) {
 	})
 }
 
-func TestGetRedisKey(t *testing.T) {
-	type args struct {
-		uid int64
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		{name: "any1", args: args{uid: 88}, want: "uid#88"},
-		{name: "any2", args: args{uid: 99}, want: "uid#99"},
-	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := ms.GetRedisKey(tt.args.uid); got != tt.want {
-				t.Errorf("GetRedisKey() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
--- eRedis/internal/model/user.go
+++ eRedis/internal/model/user.go
@@ -1,74 +1,7 @@
 package model
 
-import (
-	"fmt"
-	"math/rand"
-	"strconv"
-	"time"
-
-	"golang.org/x/exp/errors"
-)
-
-func init() {
-	InitUidGenerator()
-	SetUidMax(0x0FFF_FFFF_FFFF_FFFF)
-}
-
 type User struct {
 	Uid  int64  `redis:"uid" validate:"required,gte=0"`
 	Name string `redis:"name" validate:"required,min=1,max=18"`
 	Sex  int64  `redis:"sex" validate:"required,gte=1,lte=2"`
-}
-
-//
-func GetRedisKey(uid int64) string {
-	return "uid#" + strconv.FormatInt(uid, 10)
-}
-
-//
-var uidmax int64 = 0x0FFF_FFFF_FFFF_FFFF
-
-func InitUidGenerator() {
-	rand.Seed(time.Now().UnixNano())
-}
-func GetUid() int64 {
-	return rand.Int63n(uidmax)
-}
-func SetUidMax(max int64) {
-	uidmax = max
-}
-func GetUidMax() int64 {
-	return uidmax
-}
-
-// for cache
-var expire int64 = 10
-
-//
-func GetExpire() int64 {
-	return expire
-}
-
-//
-func SetExpire(time int64) {
-	expire = time
-}
-
-//
-var ErrArgError = errors.New("arg error")
-
-var ErrUidError = fmt.Errorf("uid %w", ErrArgError)
-var ErrNameError = fmt.Errorf("name %w", ErrArgError)
-var ErrSexError = fmt.Errorf("sex %w", ErrArgError)
-
-var UserErrMap = map[string]error{
-	"Uid":  ErrUidError,
-	"Name": ErrNameError,
-	"Sex":  ErrSexError,
-}
-
-var UserEcodeMap = map[string]int64{
-	"Uid":  10001,
-	"Name": 10002,
-	"Sex":  10003,
 }
