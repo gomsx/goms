cf301c1f2d33ae0f517d469deaf7071afad7820a
vuca <aivuca@163.com>
2021-01-10 16:41:28 +0800

    chore(tools/hooks): update with debug print

--- tools/hooks/check-code-go.sh
+++ tools/hooks/check-code-go.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 # set -x
-set -e
+# set -e
 
 echo -e "==> start check code go ..."
 
-# go 源码静态分析
-cd "$1"
-for f in $2; do
-    go fmt $f
+files="$1"
+
+pkgs=$(echo "$files" | xargs dirname | sort -u)
+
+for pkg in $pkgs; do
+    go fmt ./"$pkg"/...
+    go vet ./"$pkg"/...
 done
-go mod tidy
+
+# go mod tidy
 
 # 逃逸分析，'-l' 防止内联
 # go build -gcflags '-m -l'
--- tools/hooks/check-doc.sh
+++ tools/hooks/check-doc.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 # set -x
-set -e
+# set -e
 
 echo -e "==> start check doc ..."
 
-cd "$1"
+files="$1"
+
 # 处理改动的文件
-for f in $2; do
-	# 匹配空格、tab等特殊字符,替换成换行符
-	sed -i 's/^\s*$/\n/g' "$f"
-	# 尾行部插入空行
-	sed -i '$a\\n' "$f"
+for file in $files; do
+	# 行尾，删除行尾空格
+	[ "${file##*.}" != "md" ] && (sed -i 's/ *$//' "$file")
+	
+	# 行首，替换特殊字符(空格、tab等)成换行符
+	sed -i 's/^\s*$/\n/g' "$file"
+
+set -x
+	# 尾行，任意不是'\n'(正文),则追加'\n'(换行)
+	# ecoh -n 不附加换行符
+	# 字符串判断(=~ 正则表达式)(== 通配符)
+	[[ "$(tail -1 "$file")" =~ .+[^\n]$ ]] && (echo -e -n "\n" >>  "$file")
+	# [[ "$(tail -1 "$file")" =~ .+[^\n]$ ]] && (echo -e -n "\n" >>  "$file") && echo "===> ok"
+	
+	# 尾行，是'\n',则删除
+	while [ "$(wc -l < "$file")" -gt 0 ] && [ "$(tail -1 "$file")" == "" ]; do
+		mv "$file" "$file".bak
+		tac "$file".bak | sed '1d' | tac > "$file"
+		# (tac "$file".bak | sed '1d' | tac > $file) && echo "---> ok"
+		rm -f "$file".bak
+	done
+set +x
+
+	# 首行，删除为空的首行
+	sed -i '/./,$!d' "$file"
+
 	# 合并多个空行
-	sed -i '/^$/{N;/^\n*$/D}' "$f"
-	# 删除为空的首行
-	sed -i '/./,$!d' "$f"
+	sed -i '/^$/{N;/^\n*$/D}' "$file"	
 done
 
 echo -e "==< end check doc"
