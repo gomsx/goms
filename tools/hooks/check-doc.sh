cec448ef72f9522dec7fbc4835c1f68c8a0d6a57
vuca <aivuca@163.com>
2021-01-12 11:34:55 +0800

    chore(tools/hooks): update without debug print

--- tools/hooks/check-doc.sh
+++ tools/hooks/check-doc.sh
@@ -14,21 +14,17 @@ for file in $files; do
 	# 行首，替换特殊字符(空格、tab等)成换行符
 	sed -i 's/^\s*$/\n/g' "$file"
 
-set -x
-	# 尾行，任意不是'\n'(正文),则追加'\n'(换行)
+	# 尾行 1，任意不是'\n'(正文),则追加'\n'(换行)
 	# ecoh -n 不附加换行符
 	# 字符串判断(=~ 正则表达式)(== 通配符)
 	[[ "$(tail -1 "$file")" =~ .+[^\n]$ ]] && (echo -e -n "\n" >>  "$file")
-	# [[ "$(tail -1 "$file")" =~ .+[^\n]$ ]] && (echo -e -n "\n" >>  "$file") && echo "===> ok"
 	
-	# 尾行，是'\n',则删除
+	# 尾行 2，是'\n',则删除
 	while [ "$(wc -l < "$file")" -gt 0 ] && [ "$(tail -1 "$file")" == "" ]; do
 		mv "$file" "$file".bak
 		tac "$file".bak | sed '1d' | tac > "$file"
-		# (tac "$file".bak | sed '1d' | tac > $file) && echo "---> ok"
 		rm -f "$file".bak
 	done
-set +x
 
 	# 首行，删除为空的首行
 	sed -i '/./,$!d' "$file"
