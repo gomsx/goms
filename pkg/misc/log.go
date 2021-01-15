6fa2746801cf92d3260e611e8f5ed5d08441a6e3
sfw <sfuwen@sina.com>
2021-01-15 19:07:00 +0800

    refactor: move xxLogxx() to pkg/misc for eApi

--- pkg/misc/log.go
+++ pkg/misc/log.go
@@ -3,6 +3,7 @@ package misc
 import (
 	"context"
 
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
@@ -21,4 +22,19 @@ func CarryCtxId(ctx context.Context, key string, val int64) context.Context {
 	// l := log.With().Int64(key, val).Logger() // 丢失 ctx 中的 (key，val)
 	l := log.Ctx(ctx).With().Int64(key, val).Logger() // 保存 ctx 中的 (key，val)
 	return l.WithContext(ctx)
+}
+
+//
+func GetLogLevel() string {
+	level := zerolog.GlobalLevel()
+	return level.String()
+}
+
+//
+func SetLogLevel(l string) {
+	level, err := zerolog.ParseLevel(l)
+	if err != nil {
+		level = zerolog.Level(zerolog.InfoLevel)
+	}
+	zerolog.SetGlobalLevel(zerolog.Level(level))
 }
