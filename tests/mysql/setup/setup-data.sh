4a108d42dc00eb6fc101b357bedece067f3faa04
vuca <aivuca@163.com>
2021-01-24 14:14:01 +0800

    test: update mysqltest to v2.2.1

--- tests/mysql/setup/setup-data.sh
+++ tests/mysql/setup/setup-data.sh
@@ -3,11 +3,10 @@ set -x
 set -e
 
 # 当前目录路径
-pwdx=$(
-	cd "$(dirname "$0")"
-	pwd
-)
+pwdx="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo "==> pwdx:$pwdx"
 
+# set db
 echo ' ==> 创建库和表...'
 mysql -uroot < "$pwdx"/test_db/create.sql
 
@@ -18,3 +17,15 @@ mysql -uroot < "$pwdx"/test_db/show_table.sql
 
 echo ' ==> 创建用户...'
 mysql -uroot < "$pwdx"/user/create.sql
+
+# # set db
+# echo ' ==> 创建库和表...'
+# mysql -uroot <test_db/create.sql
+
+# mysql -uroot <test_db/ping_table.sql
+# mysql -uroot <test_db/user_table.sql
+
+# mysql -uroot <test_db/show_table.sql
+
+# echo ' ==> 创建用户...'
+# mysql -uroot <user/create.sql
