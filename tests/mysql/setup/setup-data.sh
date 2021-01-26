01c0bcb2328f70a2cec55a287974dc60141d21a2
sfw <sfuwen@sina.com>
2021-01-26 14:12:17 +0800

    test: update mysqltest to v2.2.1

--- tests/mysql/setup/setup-data.sh
+++ tests/mysql/setup/setup-data.sh
@@ -8,15 +8,15 @@ echo "==> pwdx:$pwdx"
 
 # set db
 echo ' ==> 创建库和表...'
-mysql -uroot < "$pwdx"/test_db/create.sql
+mysql -uroot <test_db/create.sql
 
-mysql -uroot < "$pwdx"/test_db/ping_table.sql
-mysql -uroot < "$pwdx"/test_db/user_table.sql
+mysql -uroot <$pwdx/test_db/ping_table.sql
+mysql -uroot <$pwdx/test_db/user_table.sql
 
-mysql -uroot < "$pwdx"/test_db/show_table.sql
+mysql -uroot <$pwdx/test_db/show_table.sql
 
 echo ' ==> 创建用户...'
-mysql -uroot < "$pwdx"/user/create.sql
+mysql -uroot <$pwdx/user/create.sql
 
 # # set db
 # echo ' ==> 创建库和表...'
