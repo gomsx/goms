110c034a554a1697f60885cb720f2d5ede7238f9
sfw <sfuwen@sina.com>
2021-01-29 10:36:02 +0800

    tool: refactor release

--- tools/release/fix-branch.sh
+++ tools/release/fix-branch.sh
@@ -2,29 +2,36 @@
 # set -x
 set -e
 
-# VER 版本参数
-VER="$1"
+# version 版本参数
+version="$1"
 
 # 如果没有参数，打印提示并退出
-if [ -z $VER ];then
-    echo -e "❗ 错误，缺版本参数 \n格式: bash_cmd src_dir \n例子：fix-branch.sh v1.0.1"
-    exit
+if [ -z $version ]; then
+	echo -e "❗ 错误，缺版本参数 \n格式: bash_cmd version \n例子：fix-branch.sh v1.0.1"
+	exit
 else
-    echo "版本参数 $VER"
+	echo "版本参数 $version"
 fi
 
-# 当前 bash 所在目录路径 PWD
-PWD=$(cd "$(dirname "$0")";pwd)
+# 当前目录路径
+pwdx=$(
+	cd "$(dirname "$0")"
+	pwd
+)
 
-# 当前项目路径 PRO
-PRO=$PWD/../..
-PRO=$(cd $PRO;pwd)
+# 当前项目路径 pro
+pro=$(
+	cd "$pwdx/../.."
+	pwd
+)
+
+# workflow 目录
+workflow=$pro/.github/workflows
 
 # 替换版本参数
-sed -i "s/_main/_$VER/g" $PRO/README.md
-sed -i "s/\/main/\/release-$VER/g" $PRO/README.md
+sed -i "s/make_main/make_$version/g" $pro/README.md
+sed -i "s/\/tree\/main/\/tree\/release-$version/g" $pro/README.md
 
-sed -i "s/_main/_$VER/g" $PRO/.github/workflows/make_main.yml
-sed -i "s/main/release-$VER/g" $PRO/.github/workflows/make_main.yml
-mv $PRO/.github/workflows/make_main.yml $PRO/.github/workflows/make_$VER.yml
-
+sed -i "s/make_main/make_$version/g" $workflow/make_main.yml
+sed -i "s/\[ main \]/\[ release-$version \]/g" $workflow/make_main.yml
+mv $workflow/make_main.yml $workflow/make_$version.yml
