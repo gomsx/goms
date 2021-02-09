4fc4df1ef70380a441ee38fc9fdf138321b966f7
vuca <aivuca@163.com>
2021-02-09 12:15:45 +0800

    test(mock/redis): update redistest to v1.4.3

--- tests/mock/redis/setup/setup.sh
+++ tests/mock/redis/setup/setup.sh
@@ -2,14 +2,5 @@
 set -x
 set -e
 
-# 当前目录路径
-pwdx="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-echo "==> pwdx:$pwdx"
-
-# 打印环境变量
-whoami
-pwd
-which redis-server
-
 # 启动 redis 服务
-redis-server $pwdx/redis.conf
+redis-server redis.conf
