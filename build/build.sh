7f171d435a66196b36b326ce0993a336ca7d2354
sfw <sfuwen@sina.com>
2021-02-19 11:22:33 +0800

    chore(workflow): update cicd

--- build/build.sh
+++ build/build.sh
@@ -3,12 +3,12 @@ set -x
 set -e
 
 # 项目 root
-PROX="$(cd "../" && pwd)"
+PD="$(cd "../" && pwd)"
 
 # 子项目
 SUBS=("eApi" "eTest" "eRedis" "eMysql" "eConf" "eGrpc" "eHttp" "eFlag" "eYaml" "eModule" "eDocker")
 
 # work
 for ((i = 0; i < ${#SUBS[*]}; i++)); do
-	cd ${PROX}/${SUBS[i]}/build && make compile docker push clean
+	cd ${PD}/${SUBS[i]}/build && make compile docker push clean
 done
