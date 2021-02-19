e803aeefffe49b80bbc60ffa06f1236892d149fc
vuca <aivuca@163.com>
2021-02-19 14:17:06 +0800

    tool(branch): update

--- tools/branch/fix-branch.sh
+++ tools/branch/fix-branch.sh
@@ -5,51 +5,36 @@ set -e
 # branch 分支名
 branch="$1"
 
-FCMD="bash_cmd branch"
-ECMD="fix-branch.sh dev"
-HELP="❗ 分支名 \n格式: ${FCMD}\n例子: ${ECMD}"
+FCMD="格式: bash_cmd branch"
+ECMD="例子: fix-branch.sh dev"
+HELP="❗ 分支名为空"
 
 # 如果没有参数，打印提示并退出
 if [ -z "${branch}" ]; then
-	echo -e "${HELP}"
-	exit
+	echo "${HELP}"
+	echo "${FCMD}"
+	echo "${ECMD}"
+
+	branch="$(git symbolic-ref --short -q HEAD)"
+	echo "输入的分支名为空, 使用当前分支: ${branch}"
 else
-	echo "分支名 ${branch}"
+	echo "输入的分支名 ${branch}"
 fi
 
-# 项目 root
-cd ../../
-pwd
+# 工作目录路径
+WD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cicd_name="cicd-${branch}"
-cicd_file="cicd.yml"
+# 项目目录路径
+PD="$(cd "$WD/../.." && pwd)"
 
 # README
 ## badge
-old="workflows\/[^\/]*\/badge"
-new="workflows\/${cicd_name}\/badge"
-sed -i "s/${old}/${new}/g" ./README.md
-
-old="workflow%3A.*)"
-new="workflow%3A${cicd_name})"
-sed -i "s/${old}/${new}/g" ./README.md
+sub="\/badge.svg?branch="
+old="[^)]*)"
+new="${branch})"
+(cd ${PD} && sed -i "s/\(${sub}\)\(${old}\)/\1${new}/g" ./README.md)
 
 ## branch tree
 old="tree\/[^\/]*\/"
 new="tree\/${branch}\/"
-sed -i "s/${old}/${new}/g" ./README.md
-
-# workflow 目录
-cd .github/workflows
-pwd
-
-## cicd
-cicd="cicd.yml"
-old="^name\: .*$"
-new="name\: ${cicd_name}"
-sed -i "s/${old}/${new}/g" ${cicd_file}
-
-## .yml
-old="branches: \[ .* \]"
-new="branches: \[ ${branch} \]"
-sed -i "s/${old}/${new}/g" "$(grep -rl -E "${old}")"
+(cd ${PD} && sed -i "s/${old}/${new}/g" ./README.md)
