f9a26f2d00d5ee98d81456018ce9022d8b430c4d
sfw <sfuwen@sina.com>
2021-02-21 12:58:18 +0800

    test(tests/mock): update mysqltest v2.2.2

--- tests/mock/mysql/setup/setup.sh
+++ tests/mock/mysql/setup/setup.sh
@@ -2,15 +2,6 @@
 set -x
 set -e
 
-# 当前目录路径
-pwdx="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-echo "==> pwdx:$pwdx"
-
-# 打印环境变量
-whoami
-pwd
-which mysql mysqld
-
 # 设置数据库
 echo ' ===> 1.启动 mysql...'
 service mysql start
@@ -19,10 +10,8 @@ service mysql start
 service mysql status
 
 echo ' ===> 2.创建库和表...'
-bash "$pwdx"/setup-data.sh
+bash setup-data.sh
 echo '导入完毕...'
 
 service mysql status
 echo ' ===> 3.初始化完成...'
-
-tail -f /dev/null
