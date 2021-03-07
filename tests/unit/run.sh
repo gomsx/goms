049fe3912dce5bc906228e22d5baef15e38d5ce9
sfw <sfuwen@sina.com>
2021-03-07 15:01:59 +0800

    test: remove v flag for go test

--- tests/unit/run.sh
+++ tests/unit/run.sh
@@ -6,6 +6,6 @@ set -e
 PD="$(cd ../../ && pwd)"
 echo "==> pro dir: ${PD}"
 
-cd ${PD}/pkg; go test -v -gcflags=-l -count=1 ./...
+cd ${PD}/pkg; go test -gcflags=-l -count=1 ./...
 cd ${PD}/eApi/build; make test
 cd ${PD}/eTest/build; make test
